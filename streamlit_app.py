4d95ec65150fe211a469bf2943d6a25cf32cd642
NUR Digital Marketing <163868030+nurdigitalmarketing@users.noreply.github.com>
2024-03-18 17:03:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ if get_gsc_file is not None:
 
     for index, row in df.iterrows():
       keyword = row['Query più frequenti']
-      pytrends = TrendReq(hl='it-IT', tz=360)
+      pytrends = TrendReq(hl='en-US', tz=360)
       kw_list = [keyword]
       pytrends.build_payload(kw_list, cat=0, timeframe=timeframe, geo=geo, gprop='')
       df2 = pytrends.interest_over_time()
